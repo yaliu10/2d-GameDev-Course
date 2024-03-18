8f9736a471619d579037206fefe324ab745c7766
yaliu10 <yaliu10@s.sfusd.edu>
2024-03-18 11:52:45 -0700

    L4 and practice

--- 04.to_space_and_beyond_workbook/lessons/ship.gd
+++ 04.to_space_and_beyond_workbook/lessons/ship.gd
@@ -1,13 +1,19 @@
 extends Sprite2D
 
 var velocity := Vector2(0,0)
-var max_speed := 600.0
+var normal_speed := 600.0
+var max_speed := normal_speed
+var boost_speed := 1500.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var direction := Vector2(0,0)
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
+	
+	if Input.is_action_just_pressed("boost"):
+		max_speed = boost_speed
+		get_node("Timer").start()
 	
 	velocity = direction * max_speed
 	position += velocity * delta
@@ -17,4 +23,7 @@ func _process(delta: float) -> void:
 	
 	if direction.length() > 1.0:
 		direction = direction.normalized()
-	
+		
+
+func _on_timer_timeout() -> void:
+	max_speed = normal_speed
