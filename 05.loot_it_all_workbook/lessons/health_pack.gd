df9712f20f1f32ba1c27caa0a9798eb9381316f1
yaliu10 <yaliu10@s.sfusd.edu>
2024-03-26 09:49:04 -0700

    L1 and L2 and practice completed

--- 05.loot_it_all_workbook/lessons/health_pack.gd
+++ 05.loot_it_all_workbook/lessons/health_pack.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
-func _on_area_entered(area_that_entered: Area2D) -> void:
-	queue_free()
-
 func _ready() -> void:
 	area_entered.connect(_on_area_entered)
+
+func _on_area_entered(area_that_entered: Area2D) -> void:
+	queue_free()
