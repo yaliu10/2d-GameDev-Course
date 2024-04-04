b833df2fe4af82ba8fe96c5d7da7f1455ea84ace
yaliu10 <yaliu10@s.sfusd.edu>
2024-04-04 13:37:32 -0700

    L6, Practice 1 and 2 completed

--- 05.loot_it_all_workbook/lessons/ship.gd
+++ 05.loot_it_all_workbook/lessons/ship.gd
@@ -1,8 +1,11 @@
 extends Area2D
 
+var gem_count := 0
+
 var max_speed := 1200.0
 var velocity := Vector2(0, 0)
 var steering_factor := 3.0
+
 var health := 10
 
 func _ready() -> void:
@@ -30,5 +33,11 @@ func set_health(new_health: int) -> void:
 	get_node("UI/HealthBar").value = health
 
 func _on_area_entered(area_that_entered: Area2D) -> void:
-	set_health(health+10)
+	if area_that_entered.is_in_group("gem"):
+		set_gem_count(gem_count + 1)
+	elif area_that_entered.is_in_group("healing_item"):
+		set_health(health + 10)
 
+func set_gem_count(new_gem_count: int) -> void:
+	gem_count = new_gem_count
+	get_node("UI/GemCount").text = "x" + str(gem_count)
--- 05.loot_it_all_workbook/practices/L6.P2.coins_and_energy_packs/ship_coins_and_energy_packs.gd
+++ 05.loot_it_all_workbook/practices/L6.P2.coins_and_energy_packs/ship_coins_and_energy_packs.gd
@@ -34,7 +34,17 @@ func _process(delta: float) -> void:
 		set_process(false)
 		target_reached.emit()
 
+func set_energy(new_energy: int) -> void:
+	energy = new_energy
+	get_node("UI/EnergyBar").value = energy
+
+func set_coins(new_coins: int) -> void:
+	coins = new_coins
+	get_node("UI/CoinsCount").text = "x" + str(coins)
+
 
 func _on_area_entered(area_that_entered: Area2D) -> void:
-	energy += 20.0
-	get_node("UI/EnergyBar").value = energy
+	if area_that_entered.is_in_group("energy"):
+		set_energy(energy + 20)
+	elif area_that_entered.is_in_group("coin"):
+		set_coins(coins + 1)
