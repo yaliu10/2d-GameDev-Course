b65c64ec916b9b6f2199fbdcc4a3b10597b4d6e1
yaliu10 <yaliu10@s.sfusd.edu>
2024-04-15 11:42:16 -0700

    L9 completed

--- 05.loot_it_all_workbook/lessons/ship.gd
+++ 05.loot_it_all_workbook/lessons/ship.gd
@@ -25,6 +25,10 @@ func _process(delta: float) -> void:
 	velocity += steering * steering_factor * delta
 	position += velocity * delta
 
+	var viewport_size := get_viewport_rect().size
+	position.x = wrapf(position.x, 0, viewport_size.x)
+	position.y = wrapf(position.y, 0, viewport_size.y)
+
 	if velocity.length() > 0.0:
 		get_node("Sprite2D").rotation = velocity.angle()
 
