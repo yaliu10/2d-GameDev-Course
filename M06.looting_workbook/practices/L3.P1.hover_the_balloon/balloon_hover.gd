6bc9981f57bce7172653ce36b2e4d22b84c74a73
yaliu10 <yaliu10@s.sfusd.edu>
2024-04-22 11:53:25 -0700

    L3, practice 1 & 2 completed

--- M06.looting_workbook/practices/L3.P1.hover_the_balloon/balloon_hover.gd
+++ M06.looting_workbook/practices/L3.P1.hover_the_balloon/balloon_hover.gd
@@ -5,6 +5,8 @@ extends Area2D
 
 
 func _ready() -> void:
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
 	set_outline_thickness(5.0)
 
 
@@ -14,8 +16,7 @@ func set_outline_thickness(new_thickness: float) -> void:
 
 
 func _on_mouse_entered() -> void:
-	pass
-
+	set_outline_thickness(10.0) 
 
 func _on_mouse_exited() -> void:
-	pass
+	set_outline_thickness(5.0) 
