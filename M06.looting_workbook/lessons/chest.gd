b9180cb592f6860e25c15d4a1f93fe9c7e32c20b
yaliu10 <yaliu10@s.sfusd.edu>
2024-04-29 11:17:00 -0700

    L5 and practice 1 & 2 completed

--- M06.looting_workbook/lessons/chest.gd
+++ M06.looting_workbook/lessons/chest.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @onready var canvas_group: CanvasGroup = $CanvasGroup
+@onready var animation_player = $AnimationPlayer
 
 
 func _ready() -> void:
@@ -18,3 +19,18 @@ func _on_mouse_entered() -> void:
 func _on_mouse_exited() -> void:
 	var tween := create_tween()
 	tween.tween_method(set_outline_thickness, 6.0, 3.0, 0.08)
+
+func _input_event(viewport: Node, event: InputEvent, shape_idx: int):
+	var event_is_mouse_click: bool = (
+		event is InputEventMouseButton and 
+		event.button_index == MOUSE_BUTTON_LEFT and
+		event.is_pressed()
+	)
+
+	if event_is_mouse_click:
+		open()
+
+func open() -> void:
+	animation_player.play("open")
+
+	input_pickable = false
